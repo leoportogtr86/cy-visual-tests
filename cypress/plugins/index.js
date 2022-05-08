ddb83a4f21813b40a2bee5ae1b9e550166486808
leoportogtr86 <leoporto_studio@hotmail.com>
2022-05-08 09:49:37 -0300

    0

---
+++ cypress/plugins/index.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+
+const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
+
+module.exports = (on, config) => {
+  addMatchImageSnapshotPlugin(on, config)
+}
